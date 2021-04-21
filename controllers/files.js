1a72ce191e6e028645dba90f4dafbeea06ae7e3a
Eric Manongdo <superscript@Erics-MacBook-Pro.local>
2021-04-20 18:26:34 -1000

    Initial commit with upload functionality

---
+++ controllers/files.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const File = require("../model/file");
+
+export const postUpload = async (req, res) => {
+  //   console.log("BODY---->", req.body);
+  //   console.log("FILE---->", req.file);
+  try {
+    const { title, category } = req.body;
+    const { path, mimetype } = req.file;
+    const file = new File({
+      title,
+      category,
+      file_path: path,
+      file_mimetype: mimetype,
+    });
+    await file.save();
+    res.send("file uploaded successfully.");
+  } catch (error) {
+    res.status(400).send("Error while uploading file. Try again later.");
+  }
+};
+
+export const getAllFiles = async (req, res) => {
+  try {
+    const { category } = req.body;
+    const files = await File.find({ category: category });
+    const sortedByCreationDate = files.sort(
+      (a, b) => b.createdAt - a.createdAt
+    );
+    res.send(sortedByCreationDate);
+  } catch (error) {
+    res.status(400).send("Error while getting list of files. Try again later.");
+  }
+};
+
+export const download = async (req, res) => {
+  try {
+    const file = await File.findById(req.params.id);
+    res.set({
+      "Content-Type": file.file_mimetype,
+    });
+    res.sendFile(path.join(__dirname, "..", file.file_path));
+  } catch (error) {
+    res.status(400).send("Error while downloading file. Try again later.");
+  }
+};
---
+++ routes/file.js
@@ -0,0 +1,40 @@
+import express from "express";
+const multer = require("multer");
+const router = express.Router();
+
+// middlewares
+// TODO
+
+// controllers
+import { postUpload, getAllFiles, download } from "../controllers/files";
+
+// multer upload function
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "./files");
+    },
+    filename(req, file, cb) {
+      cb(null, `${new Date().getTime()}_${file.originalname}`);
+    },
+  }),
+  limits: {
+    fileSize: 1000000, // max file size 1MB = 1000000 bytes
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpeg|jpg|png|pdf|doc|docx|xlsx|xls)$/)) {
+      return cb(
+        new Error(
+          "only upload files with jpg, jpeg, png, pdf, doc, docx, xslx, xls format."
+        )
+      );
+    }
+    cb(undefined, true); // continue with upload
+  },
+});
+
+router.post("/upload", upload.single("file"), postUpload);
+router.post("/getAllFiles", getAllFiles);
+router.get("/download/:id", download);
+
+module.exports = router;
