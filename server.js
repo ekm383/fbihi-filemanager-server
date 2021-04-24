cc0a662156f6a9a5cfe0f5e4c5ee40179b8687a9
Eric Manongdo <superscript@Erics-MacBook-Pro.local>
2021-04-23 18:51:22 -1000

    server,js update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from "express";
+const express = require("express");
 import { readdirSync } from "fs";
-import cors from "cors";
-import mongoose from "mongoose";
+const cors = require("cors");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const fileRoute = require("./routes/file");
 require("dotenv").config();
@@ -20,13 +20,14 @@ mongoose
   .catch((err) => console.log("DB Connection Error:", err));
 
 // middlewares
-app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(fileRoute);
+app.use(cors());
 
 // route middlewares
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
